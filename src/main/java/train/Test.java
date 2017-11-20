303fb229b8858ccef13e2b586f39fb29e7b6ce86
crazyyanchao <1141679423@qq.com>
2017-11-20 23:18:31 +0800

    add test3.txt

--- src/main/java/train/Test.java
+++ src/main/java/train/Test.java
@@ -24,4 +24,12 @@ package train;
  * 　　　　　　　　　 ┗┻┛　 ┗┻┛+ + + +
  */
 public class Test {
+    private void run() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.run();
+    }
 }
