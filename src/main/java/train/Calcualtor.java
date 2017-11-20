22596a14e335777dabdcfce05f1f2413f9b7fbea
crazyyanchao <1141679423@qq.com>
2017-11-20 22:54:14 +0800

    add test3.txt

---
+++ src/main/java/train/Calcualtor.java
@@ -0,0 +1,41 @@
+package train;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 　　　　　　　 ┏┓       ┏┓+ +
+ * 　　　　　　　┏┛┻━━━━━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　 ┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 █████━█████  ┃+
+ * 　　　　　　　┃　　　　　　 ┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　 ┃ + +
+ * 　　　　　　　┗━━┓　　　 ┏━┛
+ * ┃　　  ┃
+ * 　　　　　　　　　┃　　  ┃ + + + +
+ * 　　　　　　　　　┃　　　┃　Code is far away from     bug with the animal protecting
+ * 　　　　　　　　　┃　　　┃ +
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　 ┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━━━┳┓┏┛ + + + +
+ * 　　　　　　　　　 ┃┫┫　 ┃┫┫
+ * 　　　　　　　　　 ┗┻┛　 ┗┻┛+ + + +
+ */
+public class Calcualtor {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 1000; i++) {
+            executorService.execute(new Runnable() {
+                public void run() {
+                    System.out.println("Hello world!");
+
+                }
+            });
+        }
+    }
+}
